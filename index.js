ae4b4389690723ddd0f621d5e8a21ecd4884f990
099s <108814884+X4ni@users.noreply.github.com>
2024-08-28 18:40:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Ticket System","OceanBox on top"];
 
 
 let currentIndex = 0;
